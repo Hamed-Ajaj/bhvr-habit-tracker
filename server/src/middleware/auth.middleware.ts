843c5429dc4572ba8defc1a095490bbdc412420d
hamed ajaj <hamedsgaming@gmail.com>
2025-08-24 16:30:11 +0300

    fix type error

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -2,10 +2,15 @@ import { createMiddleware } from 'hono/factory';
 import { auth } from '../lib/auth';
 import type { HonoEnv } from '@server/types';
 
-export const authMiddleware = createMiddleware(async (c, next) => {
-  if (c.req.method === "OPTIONS") return c.text("ok");
+export const authMiddleware = createMiddleware<HonoEnv>(async (c, next) => {
+  console.log("Request headers:", c.req.raw.headers);
   const session = await auth.api.getSession({ headers: c.req.raw.headers });
-  if (!session) return c.json({ error: "Unauthorized" }, 401);
-  c.set("user", session.user);
+
+  if (!session) {
+    return c.json({ error: 'Unauthorized' }, 401);
+  }
+
+  c.set('user', session.user);
+  c.set('session', session.session);
   return next();
 });
